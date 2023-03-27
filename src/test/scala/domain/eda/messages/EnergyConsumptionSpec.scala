121771f910c16f14c96bc1d2508a6c24898157ce
petero <obermueller.peter@gmail.com>
2023-03-27 09:53:59 +0200

    adapt test

--- src/test/scala/domain/eda/messages/EnergyConsumptionSpec.scala
+++ src/test/scala/domain/eda/messages/EnergyConsumptionSpec.scala
@@ -6,17 +6,18 @@ import org.scalatest.wordspec.AnyWordSpec
 
 import java.io.{FileInputStream, InputStream}
 import scala.util.{Failure, Success}
-
 import io.circe.generic.auto._
 import io.circe.syntax._
 import io.circe.parser._
-
 import at.energydash.model.JsonImplicit._
+
+import scala.io.Source
 
 class EnergyConsumptionSpec extends AnyWordSpec {
   "Energy XML File" should {
     "Parse from XML" in {
-      val xmlFile = scala.xml.XML.load(new FileInputStream("/home/petero/projects/energycash/xml/DATEN_CRMSG/message-220820_143600.xml"))
+//      val xmlFile = scala.xml.XML.load(new FileInputStream("/home/petero/projects/energycash/xml/DATEN_CRMSG/message-daten_crmsg.xml"))
+      val xmlFile = scala.xml.XML.load(Source.fromResource("message-daten_crmsg.xml").reader())
       println(ConsumptionRecordMessage.fromXML(xmlFile) match {
         case Success(m) => m.asJson.deepDropNullValues.noSpaces.toString()
         case Failure(exception) => exception.toString
